aa3a95f26e129e60a1101b09b4e5b3dae64f2ecf
Patrick Ly <patrickkimly@gmail.com>
2019-08-30 09:16:55 -0700

    Declare all elements

---
+++ index.js
@@ -0,0 +1,7 @@
+var notify = document.querySelector('.notify');
+var albums = document.querySelectorAll('.album');
+var itemsTotal = document.querySelector('.items-total');
+var saveBtn = document.querySelector('.button');
+
+
+console.log(albums);
