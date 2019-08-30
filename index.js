eb7c6363b52650291c75c0f2f9863b638ab63845
Patrick Ly <patrickkimly@gmail.com>
2019-08-30 09:51:31 -0700

    Looping in the while

--- index.js
+++ index.js
@@ -3,5 +3,18 @@ var albums = document.querySelectorAll('.album');
 var itemsTotal = document.querySelector('.items-total');
 var saveBtn = document.querySelector('.button');
 
+var albumsSelected = [];
+var i = 0;
 
-console.log(albums);
+while(i < albums.length){
+    albums[i].onclick = function(e) {
+        var albumTitle = this.querySelector('.title').textContent;
+        console.log('clicked an album: ', albumTitle);
+        console.log('this title:', this.querySelector('.title').textContent);
+        console.log('this:', this);
+
+    }
+    i++;
+}
+
+// console.log(albums);
